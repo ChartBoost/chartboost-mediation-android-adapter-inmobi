c107de17374ec8dab1d6440c072d5117ed8efbc1
bichenwang <bichenwang@gmail.com>
2023-03-07 14:15:52 -0800

    HB-5107 InMobi to 10.1.3 (#34)

--- InMobiAdapter/build.gradle.kts
+++ InMobiAdapter/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.10.1.1.1"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.10.1.3.0"
         buildConfigField("String", "CHARTBOOST_MEDIATION_INMOBI_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -70,7 +70,7 @@ dependencies {
     "remoteImplementation"("com.chartboost:chartboost-mediation-sdk:4.+")
 
     // Partner SDK
-    implementation("com.inmobi.monetization:inmobi-ads:10.1.1")
+    implementation("com.inmobi.monetization:inmobi-ads:10.1.3")
 
     //Partner SDK Dependencies
     implementation("androidx.appcompat:appcompat:1.5.1")
