4b20533cc63122501827a71489759ccfe00e1eeb
Ray Graham <rayg@bitwisedesign.com>
2024-09-05 09:54:01 -0700

    Reverted back to targetSdk 34; added compile options and buildConfig=true for gradle8 compatibility

--- InMobiAdapter/build.gradle.kts
+++ InMobiAdapter/build.gradle.kts
@@ -29,11 +29,11 @@ repositories {
 
 android {
     namespace = "com.chartboost.mediation.inmobiadapter"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
         android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.10.7.7.0"
         buildConfigField("String", "CHARTBOOST_MEDIATION_INMOBI_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
@@ -59,8 +59,14 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
